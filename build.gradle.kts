e0bc0fc35065d688dc3390417e1697eb3ecba1a1
Brian Takita <brian.takita@gmail.com>
2026-02-05 14:51:39 -0500

    Version 0.3.2: Fix compatibility issues with JetBrains Marketplace.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,15 @@ dependencies {
     }
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 kotlin {
-    jvmToolchain(21)
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 intellijPlatform {
